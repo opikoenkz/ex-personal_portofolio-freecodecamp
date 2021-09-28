e61f0dd68176fe9b4889154f9bef1d5e2312cf52
opikoenkz <opikoenkz@gmail.com>
2021-09-28 11:14:03 +0700

    update kedua

--- asset/main.js
+++ asset/main.js
@@ -1,29 +1,6 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    //sticky navbar on scroll script
-    if ($(window).scrollTop()) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
-
-    //scroll-up button show/hide script
-    /*if(this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        }else {
-            $('.scroll-up-btn').removeClass("show");
-        }*/
-  });
-
-  /*$('.navbar .navbar-menu .navbar-nav li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });*/
-});
-
 /*----- Navbar Hamburger -----*/
 const menuBtn = document.querySelector(".menu-btn");
-const menuItems = document.querySelector(".navbar-nav");
+const menuItems = document.querySelector(".navbar-menu");
 const menuItem = document.querySelectorAll(".nav-link");
 
 // main toggle
